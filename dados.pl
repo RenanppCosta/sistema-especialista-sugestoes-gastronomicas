06ca1bb82705c01c8cbefb3062be564d338e8a0f
Nicole de Oliveira Medeiros <nick.rjo@gmail.com>
2024-11-22 12:22:45 -0300

    adição dos pratos que estavam faltando no .pl

--- dados.pl
+++ dados.pl
@@ -1,27 +1,53 @@
 % Base de dados de pratos e ingredientes
+ingredientes(nhoque_a_bolonhesa, [batata, farinha_de_trigo, ovos, molho_de_tomate, cebola, alho, azeite, sal, pimenta]).
+ingredientes(curry_de_frango, [peito_de_frango, leite_de_coco, curry, cebola, alho, gengibre, tomate, espinafre, sal, pimenta]).
+ingredientes(lasanha_com_berinjela, [berinjela, molho_de_tomate, carne_moida, queijo_mussarela, queijo_parmesao, alho, cebola, azeite, sal, pimenta]).
+ingredientes(salada_de_quinoa_com_frango, [quinoa, peito_de_frango, alface, tomate, pepino, cenoura, azeite, limao, sal, pimenta]).
+ingredientes(risoto_de_cogumelos, [arroz_arboreo, cogumelos, cebola, alho, vinho_branco, caldo_de_legumes, queijo_parmesao, manteiga]).
+ingredientes(peixe_assado_com_legumes, [file_de_peixe, batata, cenoura, brocolis, azeite, limao, alho, sal, pimenta]).
+ingredientes(macarrao_a_bolonhesa, [macarrao, carne_moida, molho_de_tomate, cebola, alho, azeite, sal, pimenta, manjericao]).
+ingredientes(estrogonofe_de_frango, [peito_de_frango, creme_de_leite, ketchup, mostarda, cebola, alho, champignon, sal, pimenta]).
+ingredientes(torta_de_frango, [frango, massa_de_torta, cebola, alho, tomate, milho, ervilha, creme_de_leite, sal, pimenta]).
+ingredientes(feijoada, [feijao_preto, carne_seca, linguica, bacon, cebola, alho, louro, sal, pimenta]).
+ingredientes(bife_a_parmegiana, [bife, farinha_de_rosca, ovo, molho_de_tomate, queijo_mussarela, sal, pimenta, oleo]).
+ingredientes(salmao_grelhado_com_aspargos, [file_de_salmao, aspargos, azeite, limao, alho, sal, pimenta]).
+ingredientes(cuscuz_marroquino_com_legumes, [cuscuz_marroquino, abobrinha, cenoura, pimentao, cebola, alho, azeite, sal, pimenta]).
+ingredientes(frango_xadrez, [peito_de_frango, pimentao, cebola, molho_de_soja, amido_de_milho, alho, gengibre, sal, pimenta]).
+ingredientes(carne_de_panela_com_batatas, [carne_de_panela, batata, cenoura, cebola, alho, tomate, louro, sal, pimenta]).
+ingredientes(quiche_de_espinafre_com_ricota, [espinafre, ricota, massa_de_quiche, ovos, creme_de_leite, cebola, alho, sal, pimenta]).
+ingredientes(moqueca_de_peixe, [file_de_peixe, leite_de_coco, azeite_de_dende, pimentao, tomate, cebola, alho, coentro, sal, pimenta]).
+ingredientes(arroz_de_forno, [arroz, frango, milho, ervilha, queijo_mussarela, molho_de_tomate, cebola, alho, sal, pimenta]).
+ingredientes(polenta_ragu_carne, [polenta, carne_moida, molho_de_tomate, cebola, alho, azeite, sal, pimenta, queijo_parmesao]).
+ingredientes(sopa_de_legumes, [batata, cenoura, abobrinha, cebola, alho, caldo_de_legumes, azeite, sal, pimenta]).
+ingredientes(escondidinho_de_carne_seca, [carne_seca, mandioca, manteiga, leite, cebola, alho, queijo_coalho, sal, pimenta]).
+ingredientes(yakissoba, [macarrao, frango, brocolis, cenoura, pimentao, molho_de_soja, oleo_de_gergelim, alho, gengibre]).
+ingredientes(bacalhau_bras, [bacalhau, batata_pala, cebola, alho, ovos, azeite, azeitonas, salsa, sal, pimenta]).
+ingredientes(ratatouille, [berinjela, abobrinha, pimentao, tomate, cebola, alho, azeite, sal, pimenta, ervas_de_provence]).
+ingredientes(frango_a_passarinho, [frango, alho, limao, sal, pimenta, oleo, salsinha]).
+ingredientes(camarao_a_moranga, [camarao, abobora, creme_de_leite, requeijao, cebola, alho, azeite, sal, pimenta]).
 ingredientes(paella, [arroz, camarao, lula, mexilhao, pimentao, ervilha, acafrao, alho, cebola, azeite, sal, pimenta]).
 ingredientes(bife_acebolado, [bife, cebola, alho, azeite, sal, pimenta]).
 ingredientes(frango_quiabo, [frango, quiabo, cebola, alho, tomate, azeite, sal, pimenta]).
 ingredientes(arroz_carreteiro, [arroz, carne_seca, linguica, cebola, alho, tomate, pimentao, azeite, sal, pimenta]).
-ingredientes(lombo_assado_abacaxi, [lombo_de_porco, abacaxi, alho, cebola, azeite, sal, pimenta]).
-ingredientes(bobo_camarao, [camarao, mandioca, leite_de_coco, azeite_de_dende, cebola, alho, pimentao, tomate, coentro, sal, pimenta]).
-ingredientes(frango_milanesa, [peito_de_frango, farinha_de_rosca, ovo, farinha_de_trigo, sal, pimenta, oleo]).
+ingredientes(lombo_assado_com_abacaxi, [lombo_de_porco, abacaxi, alho, cebola, azeite, sal, pimenta]).
+ingredientes(bobo_de_camarao, [camarao, mandioca, leite_de_coco, azeite_de_dende, cebola, alho, pimentao, tomate, coentro, sal, pimenta]).
+ingredientes(frango_a_milanesa, [peito_de_frango, farinha_de_rosca, ovo, farinha_de_trigo, sal, pimenta, oleo]).
 ingredientes(caldo_verde, [batata, couve, linguica, cebola, alho, azeite, sal, pimenta]).
-ingredientes(torta_palmito, [palmito, massa_de_torta, cebola, alho, tomate, azeitona, creme_de_leite, sal, pimenta]).
+ingredientes(torta_de_palmito, [palmito, massa_de_torta, cebola, alho, tomate, azeitona, creme_de_leite, sal, pimenta]).
 ingredientes(carne_louca, [carne_de_panela, cebola, alho, pimentao, tomate, azeite, sal, pimenta]).
-ingredientes(frango_mostarda_mel, [peito_de_frango, mostarda, mel, alho, azeite, sal, pimenta]).
-ingredientes(arroz_grega, [arroz, cenoura, ervilha, milho, passas, cebola, alho, azeite, sal, pimenta]).
-ingredientes(file_peixe_molho_maracuja, [file_de_peixe, maracuja, creme_de_leite, cebola, alho, azeite, sal, pimenta]).
-ingredientes(cozido_carne_legumes, [carne_de_panela, batata, cenoura, abobrinha, cebola, alho, tomate, louro, sal, pimenta]).
-ingredientes(frango_batata_doce, [peito_de_frango, batata_doce, cebola, alho, azeite, sal, pimenta]).
-ingredientes(espaguete_alho_oleo, [espaguete, alho, azeite, sal, pimenta, salsinha]).
-ingredientes(bife_chorizo_chimichurri, [bife_de_chorizo, alho, salsinha, oregano, vinagre, azeite, sal, pimenta]).
-ingredientes(frango_laranja, [peito_de_frango, laranja, alho, cebola, azeite, sal, pimenta]).
-ingredientes(arroz_pato, [arroz, pato, cebola, alho, tomate, pimentao, azeite, sal, pimenta]).
-ingredientes(salmao_molho_maracuja, [file_de_salmao, maracuja, creme_de_leite, cebola, alho, azeite, sal, pimenta]).
-ingredientes(frango_requeijao, [peito_de_frango, requeijao, cebola, alho, azeite, sal, pimenta]).
+ingredientes(frango_com_mostarda_e_mel, [peito_de_frango, mostarda, mel, alho, azeite, sal, pimenta]).
+ingredientes(arroz_a_grega, [arroz, cenoura, ervilha, milho, passas, cebola, alho, azeite, sal, pimenta]).
+ingredientes(file_de_peixe_com_molho_de_maracuja, [file_de_peixe, maracuja, creme_de_leite, cebola, alho, azeite, sal, pimenta]).
+ingredientes(cozido_de_carne_legumes, [carne_de_panela, batata, cenoura, abobrinha, cebola, alho, tomate, louro, sal, pimenta]).
+ingredientes(frango_com_batata_doce, [peito_de_frango, batata_doce, cebola, alho, azeite, sal, pimenta]).
+ingredientes(espaguete_ao_alho_e_oleo, [espaguete, alho, azeite, sal, pimenta, salsinha]).
+ingredientes(bife_de_chorizo_com_chimichurri, [bife_de_chorizo, alho, salsinha, oregano, vinagre, azeite, sal, pimenta]).
+ingredientes(frango_com_laranja, [peito_de_frango, laranja, alho, cebola, azeite, sal, pimenta]).
+ingredientes(arroz_de_pato, [arroz, pato, cebola, alho, tomate, pimentao, azeite, sal, pimenta]).
+ingredientes(salmao_com_molho_maracuja, [file_de_salmao, maracuja, creme_de_leite, cebola, alho, azeite, sal, pimenta]).
+ingredientes(frango_com_requeijao, [peito_de_frango, requeijao, cebola, alho, azeite, sal, pimenta]).
 ingredientes(arroz_lentilhas, [arroz, lentilhas, cebola, alho, azeite, sal, pimenta]).
-ingredientes(bife_cavalo, [bife, ovo, alho, azeite, sal, pimenta]).
+ingredientes(bife_a_cavalo, [bife, ovo, alho, azeite, sal, pimenta]).
 ingredientes(frango_alho_limao, [peito_de_frango, alho, limao, azeite, sal, pimenta]).
 
 % Regras
